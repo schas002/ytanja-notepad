3eeb6e1e2f07a7fc8d2b387c168a2832cf4cb444
Andrew Zyabin [🔒] <schas002@users.noreply.github.com>
2017-06-16 17:51:11 +0300

    :sparkles: add a menu bar structure
    All items are undefined, and an issue for each is cumbersome. And don't
    say I won't experience this.

--- src/io/github/schas002/ytanja/notepad/NotepadFrame.java
+++ src/io/github/schas002/ytanja/notepad/NotepadFrame.java
@@ -3,17 +3,23 @@ package io.github.schas002.ytanja.notepad;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
 import javax.swing.border.EmptyBorder;
 
 public class NotepadFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			@Override
@@ -36,6 +42,132 @@ public class NotepadFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+
+		JMenuBar menuBar = new JMenuBar();
+		contentPane.add(menuBar, BorderLayout.NORTH);
+
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+
+		JMenuItem mntmNew = new JMenuItem("New");
+		mntmNew.setEnabled(false); // TODO
+		mntmNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,
+				InputEvent.CTRL_MASK));
+		mnFile.add(mntmNew);
+
+		JMenuItem mntmOpen = new JMenuItem("Open...");
+		mntmOpen.setEnabled(false); // TODO
+		mntmOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,
+				InputEvent.CTRL_MASK));
+		mnFile.add(mntmOpen);
+
+		JMenuItem mntmSave = new JMenuItem("Save...");
+		mntmSave.setEnabled(false); // TODO
+		mntmSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,
+				InputEvent.CTRL_MASK));
+		mnFile.add(mntmSave);
+
+		mnFile.addSeparator();
+
+		JMenuItem mntmPrint = new JMenuItem("Print...");
+		mntmPrint.setEnabled(false); // TODO
+		mntmPrint.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P,
+				InputEvent.CTRL_MASK));
+		mnFile.add(mntmPrint);
+
+		mnFile.addSeparator();
+
+		JMenuItem mntmQuit = new JMenuItem("Quit");
+		mntmQuit.setEnabled(false); // TODO
+		mntmQuit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,
+				InputEvent.CTRL_MASK));
+		mnFile.add(mntmQuit);
+
+		JMenu mnEdit = new JMenu("Edit");
+		menuBar.add(mnEdit);
+
+		JMenuItem mntmUndo = new JMenuItem("Undo");
+		mntmUndo.setEnabled(false); // TODO
+		mntmUndo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z,
+				InputEvent.CTRL_MASK));
+		mnEdit.add(mntmUndo);
+
+		JMenuItem mntmRedo = new JMenuItem("Redo");
+		mntmRedo.setEnabled(false); // TODO
+		mntmRedo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y,
+				InputEvent.CTRL_MASK));
+		mnEdit.add(mntmRedo);
+
+		mnEdit.addSeparator();
+
+		JMenuItem mntmCut = new JMenuItem("Cut");
+		mntmCut.setEnabled(false); // TODO
+		mntmCut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,
+				InputEvent.CTRL_MASK));
+		mnEdit.add(mntmCut);
+
+		JMenuItem mntmCopy = new JMenuItem("Copy");
+		mntmCopy.setEnabled(false); // TODO
+		mntmCopy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C,
+				InputEvent.CTRL_MASK));
+		mnEdit.add(mntmCopy);
+
+		JMenuItem mntmPaste = new JMenuItem("Paste");
+		mntmPaste.setEnabled(false); // TODO
+		mntmPaste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V,
+				InputEvent.CTRL_MASK));
+		mnEdit.add(mntmPaste);
+
+		JMenuItem mntmDelete = new JMenuItem("Delete");
+		mntmDelete.setEnabled(false); // TODO
+		mntmDelete
+				.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
+		mnEdit.add(mntmDelete);
+
+		JMenuItem mntmSelectAll = new JMenuItem("Select All");
+		mntmSelectAll.setEnabled(false); // TODO
+		mntmSelectAll.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,
+				InputEvent.CTRL_MASK));
+		mnEdit.add(mntmSelectAll);
+
+		mnEdit.addSeparator();
+
+		JMenuItem mntmGoToLine = new JMenuItem("Go To Line...");
+		mntmGoToLine.setEnabled(false); // TODO
+		mntmGoToLine.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_G,
+				InputEvent.CTRL_MASK));
+		mnEdit.add(mntmGoToLine);
+
+		JMenu mnFind = new JMenu("Find");
+		menuBar.add(mnFind);
+
+		JMenuItem mntmFind = new JMenuItem("Find...");
+		mntmFind.setEnabled(false); // TODO
+		mntmFind.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F,
+				InputEvent.CTRL_MASK));
+		mnFind.add(mntmFind);
+
+		JMenuItem mntmFindAndReplace = new JMenuItem("Find and Replace...");
+		mntmFindAndReplace.setEnabled(false); // TODO
+		mntmFindAndReplace.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H,
+				InputEvent.CTRL_MASK));
+		mnFind.add(mntmFindAndReplace);
+
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+
+		JMenuItem mntmHelpContents = new JMenuItem("Help Contents...");
+		mntmHelpContents.setEnabled(false); // TODO
+		mntmHelpContents.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1,
+				0));
+		mnHelp.add(mntmHelpContents);
+
+		JMenuItem mntmAboutThisApplication = new JMenuItem(
+				"About this Application...");
+		mntmAboutThisApplication.setEnabled(false); // TODO
+		mntmAboutThisApplication.setAccelerator(KeyStroke.getKeyStroke(
+				KeyEvent.VK_F1, InputEvent.CTRL_MASK));
+		mnHelp.add(mntmAboutThisApplication);
 
 		JScrollPane textAreaWrapper = new JScrollPane();
 		contentPane.add(textAreaWrapper, BorderLayout.CENTER);
